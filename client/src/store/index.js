b93a1728d441eaf45e6f4ba0fb7c14ce067a445c
Alan Zhang <gagazcfan@icloud.com>
2016-11-03 00:14:21 +0800

    disable eslint no-param-reassign for vuex mutation code.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-//import { fetchItems, fetchIdsByType, fetchUser } from './api';
+// import { fetchItems, fetchIdsByType, fetchUser } from './api';
 
 Vue.use(Vuex);
 
+/* eslint-disable no-param-reassign */
 const store = new Vuex.Store({
   state: {
     categories: [],
   },
   mutations: {
-    setCategories (state, categories) {
+    setCategories(state, categories) {
       state.categories = categories;
     },
   },
